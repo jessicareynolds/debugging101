aa6bfc101d4afccf94db2b53ba33fa6e0cf2fec8
orbitalbitmap <orbitalbitmap@gmail.com>
2020-08-13 20:27:17 -0400

    Add pug files to debug

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const { greatHouses } = require('./public/data/pugExercise02')
+
+const app = expres()
+
+app.set('view engine', 'pug')
+
+app.get('/exercise01', (request, response) => {
+  return response.render('exercise01')
+})
+
+app.get('/exercise02', (request, response) => {
+  return response.render('exercise02', greatHouses)
+})
+
+app.listen(1337, () => {
+  console.log('Listening on port: 1337...')
+})
---
+++ public/data/pugExercise02.js
@@ -0,0 +1,13 @@
+const greatHouses = [
+  { name: 'Arryn', sigil: 'Falcon' },
+  { name: 'Baratheon', sigil: 'Crowned Stag' },
+  { name: 'Greyjoy', sigil: 'Kraken' },
+  { name: 'Lannister', sigil: 'Lion' },
+  { name: 'Martell', sigil: 'Sun pierced by Spear' },
+  { name: 'Stark', sigil: 'Direwolf' },
+  { name: 'Targaryen', sigil: 'Three-headed Dragon' },
+  { name: 'Tully', sigil: 'Leaping Trout' },
+  { name: 'Tyrell', sigil: 'Rose' },
+]
+
+module.exports = { greatHouses }
